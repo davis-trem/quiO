5f7fea696ff54c05b731cd603be518959222dd5f
davis-trem <davis.trem@gmail.com>
2018-10-08 19:02:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Visualizer from './components/Visualizer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    async getData() {
+        const urls = [
+            'http://assignment.quio.com/bonding',
+            'http://assignment.quio.com/unbonding',
+            'http://assignment.quio.com/injections',
+            'http://assignment.quio.com/battery',
+            'http://assignment.quio.com/temperature',
+            'http://assignment.quio.com/errors'
+        ];
+
+        //fetches array of data
+        const promises = urls.map(async url => {
+            const res = await fetch(url);
+            const resJson = await res.json();
+            return {endpoint: url.match(/\/(\w+)$/)[1], result: resJson};
+        });
+    
+        //retrieves data from promises
+        let data = [];
+        for(let prom of promises){
+            await prom.then(x => {
+                data.push(x);
+            })
+        }
+        
+        return data;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>QuiO Data: Click one label below to toggle it from graph</p>
+                    <Visualizer data={this.getData()} />
+                    <p>Adjust Slider above to filter the time series.</p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Visualizer.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {Line} from 'react-chartjs-2';
+import {Range, createSliderWithTooltip} from 'rc-slider';
+import moment from 'moment';
+
+import 'rc-slider/assets/index.css';
+
+const RangeWithTooltip = createSliderWithTooltip(Range);
+
+class Visualizer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            fullData: [],
+            chartData: {labels:['QuiO'], datasets:[]},
+        };
+    }
+    componentDidMount(){
+        this.props.data.then(arrOfData => {
+            let {fullData, chartData} = this.state;
+            for(let data of arrOfData){
+                let color = this.randomColor();
+                fullData.push({
+                    label: data.endpoint,
+                    data: data.result.map(d => ({t: d.date, y: d.value})),
+                    backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
+                    borderColor: `rgba(${color[0]},${color[1]},${color[2]}, 0.5)`,
+                    lineTension: 0.2,
+                    fill: false,
+                    borderWidth: 3,
+                    pointRadius: 2,
+                });
+                this.setState({fullData: [...fullData]});
+            }
+           chartData.datasets = fullData;
+           this.setState({chartData});
+        });
+    }
+
+    randomColor(){
+        let blue = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let red = Math.abs(blue - green);
+        return [red, green, blue];
+    }
+
+    updateGraphRange = (values) => {
+        let {fullData, chartData} = this.state;
+        
+        for(let i = 0; i < fullData.length; i++){
+            chartData.datasets[i].data = fullData[i].data.slice(values[0], values[1]+1);
+        }
+        this.setState({chartData});        
+    }
+
+    formatRangeTip = (value)=>{
+        let today = new Date();
+        let tip = new Date(today.getFullYear()-1, today.getMonth(), today.getDate()+value);
+        return moment(tip).format('ll');
+    }
+    
+    render(){
+        return(
+            <div>
+                <Line 
+                    data={this.state.chartData}
+                    options={{
+                        legend: {
+                            labels: {
+                                fontColor: '#CCC'
+                            },
+                            display: true
+                        },
+                        responsive: true,
+                        scales: {
+                            xAxes:[{
+                                bounds: 'data',
+                                distribution: 'series',
+                                scaleLabel: {
+                                    display: true,
+                                    fontColor: '#CCC',
+                                    labelString: 'Date'
+                                },
+                                ticks: {
+                                    autoSkip: true,
+                                    fontColor: '#CCC',
+                                    maxTicksLimit: 20,
+                                    source: 'data',
+                                },
+                                time: {
+                                    unit: 'week'
+                                },
+                                type: 'time',
+                            }],
+                            yAxes:[{
+                                display: true,
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Values',
+                                    fontColor: '#CCC'
+                                }
+                            }],
+                        },
+                    }}
+                    width={1000}
+                    height={600}
+                    
+                />
+                <RangeWithTooltip
+                    min={0} 
+                    max={365}
+                    allowCross={false}
+                    defaultValue={[0,365]}
+                    onChange={this.updateGraphRange}
+                    tipFormatter={this.formatRangeTip}
+                />
+            </div>
+        );
+    }
+}
+
+export default Visualizer;
